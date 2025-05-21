573ccc500e4dbbd87253b06782d9201c88050f9a
Jake Ockerby <jakeockerby@gmail.com>
2025-05-21 18:18:17 +0100

    Update booking_tools.py

--- booking_tools.py
+++ booking_tools.py
@@ -199,7 +199,14 @@ class Booker:
         
 
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=True)
+            browser = await p.chromium.launch(browser = await p.chromium.launch(
+                headless=True,
+                args=[
+                    "--no-sandbox",
+                    "--disable-setuid-sandbox",
+                    "--disable-dev-shm-usage"
+                ]
+            ))
             context = await browser.new_context()
             page = await context.new_page()
             await stealth_async(page)
@@ -240,7 +247,7 @@ class Booker:
         #         except:
         #             continue
                 
-            await browser.close()
+            # await browser.close()
 
         # # Convert dictionary to dataframe and return
         # hotels_df = pd.DataFrame(hotels_data)
